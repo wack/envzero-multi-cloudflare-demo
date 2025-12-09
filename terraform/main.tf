2df3ad382eafc5e91032baf6a421b148e21ec537
Robbie McKinstry <robbie@wack.run>
2025-12-09 12:34:55 -0500

    Add esm support.

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,6 @@ resource "cloudflare_worker_script" "demo_worker" {
   account_id = var.cloudflare_account_id
   name       = var.worker_name
   content    = file("${path.module}/worker.js")
+  module     = true
+
 }
