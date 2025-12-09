25f0551fe7b3a166aeeb5001d387b948a4befbc6
Robbie McKinstry <robbie@wack.run>
2025-12-09 14:01:41 -0500

    Move source code into own folder.

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,6 @@ provider "cloudflare" {
 resource "cloudflare_worker_script" "demo_worker" {
   account_id = var.cloudflare_account_id
   name       = var.worker_name
-  # content    = file("${path.module}/worker.js")
-  module = true
+  content    = file("${path.module}/../app/worker.js")
+  module     = true
 }
